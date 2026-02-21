43676913e42c86227ae40a37e436b5349ca31f85
nailajabeen-byte <nailazainjabeen@gmail.com>
2026-02-21 18:51:37 +0500

    message

---
+++ Lab/Week2/application.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+main(){
+
+cout<<"          -----------------------------------------------------  "<<endl;
+cout<<"                BLOOD DONOR MANAGEMENT SYSTEM                     "<<endl;
+cout<<"         -------------------------------------------------------                                               "<<endl;
+cout<<"       FEATURES:                         "<<endl;
+cout<<"     1- Blood donor registration.      "<<endl;
+cout<<"     2- Blood group rearch             "<<endl;
+cout<<"     3- Location filter                 "<<endl;
+cout<<"     4- Admin panel               "<<endl;
+cout<<"     5- Emergency request system   "<<endl;
+cout<<"     6- Blood matching system      "<<endl;
+cout<<"     7-Analyse and reporting       "<<endl;
+cout<<"    ----------------------------------------------------------------"<<endl;
+cout<<"    Read these instructions carefully                          "<<endl;
+cout<<"    ------------------------------------------------------------"<<endl;
+
+
+
+}
